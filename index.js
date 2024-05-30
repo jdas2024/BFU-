dd48fb28b41a5b4d1cb4081ddf6da7235cc7878d
jdas2024 <jdas1986@gmail.com>
2024-05-30 19:48:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+// Del 1 Basnivå
+
+console.log('WELCOME TO THE JOKETOWN!'); 
+
+const skämt = [
+    "Why don't programmers like nature? t has too many bugs.",
+    "Why did the CSS developer go to therapy? To get rid of his margins.",
+    "How do you comfort a JavaScript developer You console them.",
+    "Why did the CSS developer leave the restaurant?Because it had no class.",
+    "Why did the JavaScript developer go missing? Because he didn't know when to return.",
+    "Why did the HTML tag go to the party? Because it wanted to break the line.",
+    "Why don't programmers like to use inline styles?  Because they want to be classy.",
+    "Why did the CSS selector break up with the HTML element? It found someone more specific.",
+    "Why did the CSS developer apply for a job? They wanted to get a position.",
+];
+
+let index = Math.floor(Math.random() * skämt.length);
+console.log(skämt[index]);
+
+
+
+// Del veckoprojket 1a, 1b +2
+
+/*console.log('WELCOME TO THE JOKETOWN!'); 
+
+const skämt =[
+    { nr:1, fråga: "Why don't programmers like nature?", svar: "It has too many bugs."},
+    { nr:2, fråga: "Why did the CSS developer go to therapy?", svar: "To get rid of his margins."},
+    { nr:3, fråga: "How do you comfort a JavaScript developer?", svar: "You console them."},
+    { nr:4, fråga: "Why did the CSS developer leave the restaurant?", svar: "Because it had no class."},
+    { nr:5, fråga: "Why did the JavaScript developer go missing?", svar: "Because he didn't know when to return."},
+    { nr:6, fråga: "Why did the HTML tag go to the party?", svar: "Because it wanted to break the line."},
+    { nr:7, fråga: "Why do JavaScript developers wear glasses?", svar: "Because they don't C#."},
+    { nr:8, fråga: "Why don't programmers like to use inline styles?", svar: "Because they want to be classy."},
+    { nr:9, fråga: "Why did the CSS selector break up with the HTML element?", svar: "It found someone more specific."},
+    { nr:10,fråga: "Why did the CSS developer apply for a job?", svar: "They wanted to get a position."},
+];
+let index = Math.floor(Math.random() * skämt.length);
+
+ let index1 = Math.floor(Math.random() * skämt.length);
+ 
+ 
+let index2;
+do {
+  index2 = Math.floor(Math.random() * skämt.length);
+} while (index2 === index1);
+
+console.log(`Joke #${skämt[index1].nr}`);
+console.log(`Question: ${skämt[index1].fråga}`);
+console.log(`Answer: ${skämt[index1].svar}`);
+console.log('---');  
+console.log(`Joke #${skämt[index2].nr}`);
+console.log(`Question: ${skämt[index2].fråga}`);
+console.log(`Answer: ${skämt[index2].svar}`);
+*/
+
+
